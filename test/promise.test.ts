a99f8f296687ba54befc061685d6a7b1d0708be7
zhangchao <zhangchao02@corp.netease.com>
2021-11-19 00:00:32 +0800

    init

---
+++ test/promise.test.ts
@@ -0,0 +1,23 @@
+import { MPromise } from '../src/codes/promise';
+
+test('promise',(done)=>{
+    // const mp = new MPromise((res, rej) => {
+    //     console.log('res')
+    //     res(1)
+    // })
+    // mp.then(s => console.log(222, s))
+    let promise = new MPromise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('1');
+        }, 500);
+    })
+     promise.then((res: any) => {
+        console.log(res);
+        expect(res).toBe('1')
+        done()
+    }, (err: any) => {
+        console.log(err);
+    })
+
+    // expect(mp.then(s => console.log(s)))
+})
